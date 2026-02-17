9707e2d9f6e2866bbf60c01a333e41c60334f889
Llego-250 <solideaze1251@gmail.com>
2026-02-17 15:27:28 +0200

    changing css path

--- index.tsx
+++ index.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './src/index.css';
+import './index.css';
 
 
 const rootElement = document.getElementById('root');
